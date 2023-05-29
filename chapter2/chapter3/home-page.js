5ceec7696aac0dde2e18214a15b7f391c234d6bf
billmazio <bmaziotis@windowslive.com>
2023-05-29 18:18:31 +0300

    home-page.html

--- chapter2/chapter3/home-page.js
+++ chapter2/chapter3/home-page.js
@@ -1,24 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var toggleButton = document.querySelector('.nav-toggle button');
+   var toggleMenu = document.querySelector('.toggle-menu');
 
-function toggleButtonHandler() {
+   toggleButton.addEventListener('click', function() {
+       if (window.innerWidth < 768) {
+           toggleMenu.style.display = (toggleMenu.style.display == 'none') ? 'block' : 'none';
+       } else {
+           toggleMenu.style.display = 'none';
+       }
+   });
 
+   // Reset the display property of the toggle menu when the window is resized
+   window.addEventListener('resize', function() {
+       if (window.innerWidth >= 768) {
+           toggleMenu.style.display = 'none';
+       }
+   });
 
-    let toggleMenu = document.querySelector('.toggle-menu');
-    let slider = document.querySelector ('.slider');
-    let main = document.querySelector ('.main');
-    let footer = document.querySelector ('.footer');
-    let toggleMenuDisplayStatus = window.getComputedStyle(toggleMenu).getPropertyValue('display');
-  
-  if (toggleMenuDisplayStatus == "none") {
-     toggleMenu.style.display = "block";
-     slider.style.display = "none";
-     main.style.display = "none";
-     footer.style.display = "none";
-  } else {
-     toggleMenu.style.display = "none";
-     slider.style.display = "block";
-     main.style.display = "block";
-     footer.style.display = "flex";
-     } 
-  
-    
-  }
+   // var searchForm = document.querySelector('.button form');
+   // var searchInput = document.querySelector('#searchInput');
+
+   searchForm.addEventListener('submit', function(event) {
+       event.preventDefault(); // prevent the form from submitting
+       var query = searchInput.value;
+       console.log("Search for: " + query); // print the search query to the console
+
+       // Place your search logic here
+   });
+});
